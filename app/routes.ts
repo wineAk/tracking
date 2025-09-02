fac5a9a675eefe4363f93798c21bc4e81507388b
Ak <36499525+wineAk@users.noreply.github.com>
2025-09-03 00:37:11 +0900

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  route("/test_wikipedia", "routes/layout.tsx", [
+  route("/tracking", "routes/layout.tsx", [
     index("routes/home.tsx"),
     route(":name", "routes/wikipedia.tsx"),
   ]),
