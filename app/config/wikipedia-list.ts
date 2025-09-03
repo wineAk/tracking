512cb9c4778696565cb0bb45accdc027c5ff5b14
wineAk <akasaka@interpark.co.jp>
2025-09-03 17:30:53 +0900

    [add] ページのリストを追加

--- app/config/wikipedia-list.ts
+++ app/config/wikipedia-list.ts
@@ -7,14 +7,19 @@ export default function wikipediaList() {
     "JSX_(JavaScript)",
     "Node.js",
     "React",
-    "Vite",
     "React_Router",
+    "Vite",
     "Tailwind_CSS",
+    "Google_Fonts",
+    "Radix_UI",
+    "Lucide",
     // ツール
     "Npm_(パッケージ管理ツール)",
+    "Git",
     "GitHub",
     "GitHub_Pages",
     "Visual_Studio_Code",
     "Cursor_(code_editor)",
+    "ChatGPT",
   ].sort((a, b) => a.localeCompare(b));
 };
