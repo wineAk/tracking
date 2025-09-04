eb231fa72e77f3800263f1a1827989e24c6a755d
Ak <36499525+wineAk@users.noreply.github.com>
2025-09-04 12:47:49 +0900

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,18 @@
 import 'dotenv/config';
 import type { Config } from "@react-router/dev/config";
+import { copyFile } from 'node:fs/promises';
+import path from 'node:path';
 const directory = process.env.VITE_REPOSITORY_NAME;
 
 export default {
   // Config options...
   basename: `/${directory}`,
+  // 404 対策
+  async buildEnd(args): Promise<void> {
+    if (!args.viteConfig.isProduction) return;
+    const buildPath = args.viteConfig.build.outDir;
+    await copyFile(path.join(buildPath, 'index.html'), path.join(buildPath, '404.html'));
+  },
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
 } satisfies Config;
