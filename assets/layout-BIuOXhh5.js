94c4f3ea83d79ca862abb1730781b2aa31c5d2bb
wineAk <wineAk@users.noreply.github.com>
2025-09-05 01:37:05 +0000

    deploy: 7f9bcfae01dde34698577e4bdc20e713a364ccdf

---
+++ assets/layout-BIuOXhh5.js
@@ -0,0 +1,5 @@
+import{w as s,p as a,O as e}from"./chunk-EF7DTUVF-C60KZCPR.js";import{S as l,T as t}from"./h1-DguANJoK.js";import"./index-BHHFSTc9.js";import"./index-UcOlxvxH.js";import"./utils-kN5vndzB.js";const m=s(function(){return a.jsx(a.Fragment,{children:a.jsx("main",{className:"h-dvh",children:a.jsx(l,{type:"always",className:`
+              h-dvh
+              [&>[data-slot='scroll-area-viewport']>div]:h-full
+              [&>[data-slot='scroll-area-scrollbar']]:pt-16
+            `,children:a.jsxs("article",{className:"h-full max-w-screen-lg mx-auto p-6  pt-16 flex flex-col gap-6",children:[a.jsx("section",{className:"pt-6",children:a.jsx(t,{className:"flex items-center justify-center gap-2",children:"HOME"})}),a.jsx(e,{})]})})})})});export{m as default};
